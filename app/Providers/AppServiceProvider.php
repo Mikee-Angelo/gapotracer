bdc69fd2213cb66a1dd28bfdc154d090360d20b5
Michael Angelo De Leon <absolutelymad18@gmail.com>
2021-07-22 18:21:53 +0800

    fixed routing for asset route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        $this->app->bind('path.public', function() {
+            return base_path().'/../public_html';
+        });
     }
 }
